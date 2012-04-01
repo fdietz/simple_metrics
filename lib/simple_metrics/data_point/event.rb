32b1726f16de6187981fe3734924c854260bfc53
Frederik Dietz <fdietz@gmail.com>
2012-04-01 12:25:55 +0200

    remove obsolete code

--- lib/simple_metrics/data_point/event.rb
+++ lib/simple_metrics/data_point/event.rb
@@ -2,16 +2,6 @@ module SimpleMetrics
   module DataPoint
     class Event < Base
 
-      class << self
-        def aggregrate_value(dps)
-          # TODO implement
-        end
-
-        def aggregrate_array(dps, name)
-          # TODO implement
-        end
-      end
-
       def initialize(attributes)
         super(attributes)
         @type = 'ev'
--- lib/simple_metrics/data_point/timing.rb
+++ lib/simple_metrics/data_point/timing.rb
@@ -2,16 +2,6 @@ module SimpleMetrics
   module DataPoint
     class Timing < Base
 
-      class << self
-        def aggregrate_value(dps)
-          # TODO implement
-        end
-
-        def aggregrate_array(dps, name)
-          # TODO implement
-        end
-      end
-
       def initialize(attributes)
         super(attributes)
         @type = 'ms'
